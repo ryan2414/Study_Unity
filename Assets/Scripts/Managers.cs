3ec053d8d9f0e7e175a0966d2f3ade67503ba645
ryan2414 <jackpar@nate.com>
2023-11-08 11:00:51 +0900

    Singleton

---
+++ Assets/Scripts/Managers.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance; // 유일성이 보장된다.
+    public static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고 온다.
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private static void Init()
+    {
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+        Managers ma = Managers.Instance;
+    }
+
+}
