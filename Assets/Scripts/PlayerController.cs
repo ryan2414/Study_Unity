028c06aa1f715cd0cc25218406c2f9c6bfbb8fa3
ryan2414 <jackpar@nate.com>
2023-11-10 21:27:28 +0900

    vector 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,39 @@
 using UnityEngine;
 
+// 1. 위치 벡터
+// 2. 방향 벡터
+struct MyVector
+{
+    public float x;
+    public float y;
+    public float z;
+
+    //        +
+    //    +   +
+    // +------+
+    public float magnitude { get { return Mathf.Sqrt(x * x + y * y + z * z); } }
+    public MyVector normalized { get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); } }
+
+    public MyVector(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
+
+    private static readonly MyVector forwardVector = new MyVector(0f, 0f, 1f);
+
+    public static MyVector operator +(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    public static MyVector operator -(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static MyVector operator *(MyVector a, float d)
+    {
+        return new MyVector(a.x * d, a.y * d, a.z * d);
+    }
+}
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
@@ -7,12 +41,23 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        
+        MyVector to = new MyVector(10f, 0f, 0f);
+        MyVector from = new MyVector(5f, 0f, 0f);
+        MyVector dir = to - from; // (5f, 0f, 0f);
+
+        dir = dir.normalized; // (1f, 0f, 0f)
+
+        MyVector newPos = from + dir * _speed;
+
+        // 방향 벡터
+            // 1. 거리(크기)    5
+            // 2. 실제 방향     ->
+
     }
 
     // GameObject (Player)
-        // Transform
-        // PlayerController (*)
+    // Transform
+    // PlayerController (*)
 
     void Update()
     {
@@ -21,8 +66,8 @@ public class PlayerController : MonoBehaviour
 
         // World -> Local
         // InverseTransformDirection
-        
-        if (Input.GetKey(KeyCode.W))    
+
+        if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * Time.deltaTime * _speed);
         }
