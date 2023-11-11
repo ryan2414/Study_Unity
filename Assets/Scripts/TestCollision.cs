00e514ff0f774f46b1aefae165985ddd508a631f
ryan2414 <jackpar@nate.com>
2023-11-11 23:29:11 +0900

    Trigger

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class TestCollision : MonoBehaviour
 {
-    // 1) 나한테 RigidBody가 있어야 한다. (IsKinematic : Off)
+    // 1) 나 혹은 상대한테 RigidBody가 있어야 한다. (IsKinematic : Off)
     // 2) 나한테 Collider가 있어야 한다. (IsTrigger : Off)
     // 3) 상대한테 Collider가 있어야 한다. (IsTrigger : Off)
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision !");    
+        Debug.Log($"Collision @ {collision.gameObject.name}");    
     }
 
-
+    // 1) 둘 다 Collider가 있어야 한다.
+    // 2) 둘 중 하나는 IsTrigger : On
+    // 3) 둘 중 하나는 RigidBody가 있어야 한다.
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger !");
+        Debug.Log($"Trigger ! {other.name}");
 
     }
 
