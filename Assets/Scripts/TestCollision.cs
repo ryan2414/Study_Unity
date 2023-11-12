1c97872ee535eeddfac116ca1f0e3836ef69d49d
ryan2414 <jackpar@nate.com>
2023-11-12 20:05:10 +0900

    Raycasting #2

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -18,7 +18,6 @@ public class TestCollision : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"Trigger ! {other.name}");
-
     }
 
     private void Update()
@@ -27,7 +26,36 @@ public class TestCollision : MonoBehaviour
 
         //Debug.Log(Input.mousePosition); // Screen
 
-        Debug.Log( Camera.main.ScreenToViewportPoint(Input.mousePosition)); // Viewport
+        //Debug.Log( Camera.main.ScreenToViewportPoint(Input.mousePosition)); // Viewport
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f, Color.red, 1.0f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
+        }
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        //    Vector3 dir = mousePos - Camera.main.transform.position;
+        //    dir = dir.normalized;
+
+        //    Debug.DrawRay(Camera.main.transform.position, dir * 100f, Color.red, 1.0f);
+
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 100f))
+        //    {
+        //        Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+        //    }
+        //}
+
     }
 
 }
