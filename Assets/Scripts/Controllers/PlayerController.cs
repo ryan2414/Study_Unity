64446a2b778adb3098472e7d27a91affe69b2d37
ryan2414 <jackpar@nate.com>
2023-11-16 21:01:11 +0900

    인벤토리 실습 #1

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -23,8 +23,6 @@ public class PlayerController : MonoBehaviour
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
 
-        Managers.UI.ShowPopupUI<UI_Button>();
-
     }
 
 
